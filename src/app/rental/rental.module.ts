40baf1c13b24c25be33604f77ae2068e2a8ac1d3
sdurrbach <sebastien.durrbach@gmail.com>
2018-08-05 15:37:50 +0200

    Add proxy config, rental detail improvement & pipes

--- src/app/rental/rental.module.ts
+++ src/app/rental/rental.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common'; // Need it to use ngFor
 import { Routes, RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { NgPipesModule } from 'ngx-pipes';
 
 import { RentalComponent } from './rental.component';
 import { RentalListComponent } from './rental-list/rental-list.component';
 import { RentalListItemComponent } from './rental-list-item/rental-list-item.component';
+import { RentalDetailComponent } from './rental-detail/rental-detail.component';
 
 import { RentalService } from './shared/rental.service';
-import { RentalDetailComponent } from './rental-detail/rental-detail.component';
+import { UppercasePipe } from '../common/pipes/uppercase.pipe';
 
 const routes: Routes = [
   {
@@ -25,11 +28,14 @@ const routes: Routes = [
     RentalComponent,
     RentalListComponent,
     RentalListItemComponent,
-    RentalDetailComponent
+    RentalDetailComponent,
+    UppercasePipe
   ],
   imports: [
     CommonModule, // Need it to use ngFor
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    HttpClientModule,
+    NgPipesModule
   ],
   providers: [
     RentalService
