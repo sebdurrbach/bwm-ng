1cf3d9778125bf246a4004e1264d92f6950c7c3b
sdurrbach <sebastien.durrbach@gmail.com>
2018-08-06 23:12:34 +0200

    Add map component & google maps API

---
+++ src/app/common/map/map.component.ts
@@ -0,0 +1,33 @@
+import { Component, Input, ChangeDetectorRef } from '@angular/core';
+import { MapService } from './map.service';
+
+@Component({
+  selector: 'bwm-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.scss']
+})
+export class MapComponent {
+
+  @Input()
+  location: string;
+  isPositionError: boolean = false;
+  lat: number;
+  lng: number;
+
+  constructor(private mapService: MapService,
+              private ref: ChangeDetectorRef) { }
+
+  mapReadyHandler() {
+    this.mapService.getGeolocation(this.location).subscribe(
+      (coordinates) => {
+        this.lat = +coordinates.lat;
+        this.lng = coordinates.lng;
+        this.ref.detectChanges();
+      },
+      (error) => {
+        this.isPositionError = true;
+      }
+    );
+  }
+
+}
---
+++ src/app/common/map/map.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AgmCoreModule } from '@agm/core';
+import { CamelizePipe } from 'ngx-pipes';
+
+import { MapComponent } from './map.component';
+import { MapService } from './map.service';
+
+@NgModule({
+  declarations: [
+    MapComponent
+  ],
+  exports: [
+    MapComponent
+  ],
+  imports: [
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyBv_PKSyRny5GOeExlSaFsmwuqWwt7bkyE'
+    }),
+    CommonModule
+  ],
+  providers: [
+    MapService,
+    CamelizePipe
+  ]
+})
+export class MapModule { }
